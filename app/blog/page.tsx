c48a24be1360d312572bddc056b452c97b64c81b
Arjun Sekhon <arjunsekhon2@gmail.com>
2025-03-19 20:49:51 +0000

    Blog title colour change

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -9,7 +9,7 @@ export const metadata = {
 export default function Page() {
   return (
     <section className="container mx-auto py-8">
-      <h1 className="font-semibold text-2xl mb-4 tracking-tighter">Blog</h1>
+      <h1 className="font-semibold text-2xl mb-4 tracking-tighter text-yellow-500">Blog</h1>
       <p className="mb-6 text-lg">
         Welcome to my blog. Here, I share insights and analysis on energy markets, data analytics, and technology. Stay tuned for updates and deep dives into the trends shaping the energy sector.
       </p>
