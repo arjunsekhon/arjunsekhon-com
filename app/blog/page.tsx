ad6cf80735e3ccda415039c353b0b3e8dd9a4840
Arjun Sekhon <arjunsekhon2@gmail.com>
2025-03-25 12:28:38 +0000

    Minor adjustment

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -11,7 +11,7 @@ export default function Page() {
     <section className="container mx-auto py-8">
       <h1 className="font-semibold text-2xl mb-4 tracking-tighter text-yellow-500">Blog</h1>
       <p className="mb-6 text-lg">
-        Welcome to my blog. Here, I share insights and analysis on energy markets, data analytics, and technology. Stay tuned for updates and deep dives into the trends shaping the energy sector.
+        Welcome to my blog. Here, I share insights and analysis on energy, power, data analytics, and technology. Stay tuned for updates and deep dives into the trends shaping the energy sector.
       </p>
       <BlogPosts />
     </section>
